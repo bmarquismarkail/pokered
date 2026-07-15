7201e59696b8b9836e0aa4b1b9ca7d81618786e6
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-15 03:13:51 +0000

    Migrate Bank 15 stat modifier names

--- wla/tools/check_linked_boundaries.py
+++ wla/tools/check_linked_boundaries.py
@@ -10,6 +10,7 @@ from pathlib import Path
 
 EXPECTED = (
     ('Vitamin Stats', 0x03, 'VitaminStats', 0x5F2E, 'VitaminStatsEnd', 0x5F51),
+    ('Stat Modifier Names', 0x0F, 'StatModTextStrings', 0x769F, 'StatModTextStringsEnd', 0x76CB),
     ('Sound Effects 1', 0x02, 'SFX_Noise_Instrument01_1_Ch8', 0x42FD, 'AudioSectionEnd1', 0x50C6),
     ('Sound Effects 2', 0x08, 'SFX_Noise_Instrument01_2_Ch8', 0x42FD, 'AudioSectionEnd2', 0x536E),
     ('Sound Effects 3', 0x1F, 'SFX_Noise_Instrument01_3_Ch8', 0x42FD, 'AudioSectionEnd3', 0x513B),
